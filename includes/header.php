705cca81b5efd543069977d9a94e037155bad50c
Rasmus Bremholm <rasmus.brem@gmail.com>
2025-12-03 11:29:17 +0100

    Basic structure.

---
+++ includes/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Rasmus Meeting Booking</title>
+</head>
+<body>
+   <h1>Boka Mötesrum</h1>
+   <nav class="navbar">
+      <a href="index.php">Startsida</a>
+      <a href="rooms.php">Mötesrum</a>
+      <a href="users.php">Personer</a>
+   </nav>
---
+++ index.php
@@ -0,0 +1,4 @@
+
+   <main class="app">
+
+   </main>
