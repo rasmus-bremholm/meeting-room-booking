782f75d59d74cd5c67c268bd73190e7c09b0ab51
Rasmus Bremholm <rasmus.brem@gmail.com>
2025-12-03 22:30:39 +0100

    Update roomManager.php

---
+++ classes/roomManager.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once __DIR__.'/room.php';
+require_once __DIR__.'/jsonFile.php';
+
+class RoomManager {
+   private JsonFile $store;
+
+   public function __construct(string $filePath){
+      $this->store = new JsonFile($filePath);
+   }
+
+   public function all(): array {return $this->store->read();}
+
+   public function findRoomById(int $id): ?array{
+      foreach($this->all() as $r){
+         if($r['id'] === $id) return $r;
+      };
+      return null;
+
+   }
+
+   public function addRoom(room $room){
+      $rooms = $this->all();
+      $maxId = 0;
+
+      foreach($rooms as $r){
+         if($r['id'] > $maxId) $maxId = $r['id'];
+      }
+      $newId = $maxId + 1;
+
+      $rooms[] = [
+         'id'=>$newId,
+         'name'=>$room->name,
+         'seats'=>$room->seats,
+         'hasTv'=>$room->hasTv,
+         'hasSound'=>$room->hasSound
+      ];
+      $this->store->write($rooms);
+   }
+   public function updateRoom(int $id, $data):?array{
+      $room = $this->findRoomById($id);
+      
+   }
+   public function deleteRoom(int $id):?array{
+
+   }
+}
