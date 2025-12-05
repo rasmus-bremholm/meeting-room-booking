5a44e439db9bc7bdb04f1c6d6fcbe04369622a81
Rasmus Bremholm <rasmus.brem@gmail.com>
2025-12-05 03:31:17 +0100

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -6,4 +6,11 @@
 
    ?>
 
-   <main class="app"></main>
+   <main class="app">
+      <div class="dashboard-container">
+         <div id="my-bookings"></div>
+         <div id="new-booking"></div>
+      </div>
+   </main>
+
+   <?php require_once __DIR__ . '/includes/footer.php'; ?>
