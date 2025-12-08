b75b58cf5f59a59f6f7196f5797590b2092b5ae6
Rasmus Bremholm <rasmus.brem@gmail.com>
2025-12-08 10:44:21 +0100

    conditionals

--- dashboard.php
+++ dashboard.php
@@ -1,14 +1,24 @@
 <?php
    require_once __DIR__ . '/includes/auth.php';
+   require_once __DIR__ . '/classes/BookingManager.php';
    $pageTitle = "Dashboard";
    $pageHeading = "Dashboard";
    require_once __DIR__ . '/includes/header.php';
 
+   $bookingManager = new BookingManager(__DIR__ . '/data/bookings.json');
+   $userBookings = $bookingManager->findBookingByUserId($_SESSION['user']['id']);
    ?>
 
    <main class="app">
       <div class="dashboard-container">
-         <div id="my-bookings"></div>
+         <div id="my-bookings">
+            <h2>Mina Bokningar</h2>
+            <?php if (empty($userBookings)): ?>
+               <p>Du har inga bokningar!</p>
+            <?php else: ?>
+               <?php ?>
+            <?php endif; ?>
+         </div>
          <div id="new-booking">
             <a class="boka-nytt-button" href="rooms.php">Boka Nytt</a>
          </div>
